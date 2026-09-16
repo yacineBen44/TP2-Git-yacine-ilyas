75c8fc986cf368bed922a082c8a68a8082022add
Yacine Ben Ammar <yacine-benammar@hotmail.com>
2026-09-16 16:34:38 +0200

    Ajout d'un message dans Main sur la branche feature-message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Projet collaboratif - TP2");
-    }
+ public static void main(String[] args) {
+ System.out.println("Projet collaboratif - TP2");
+ System.out.println("Version avec nouvelle fonctionnalité !");
+ }
 }
