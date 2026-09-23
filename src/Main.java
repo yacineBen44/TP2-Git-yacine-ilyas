7ddaad4c2df8d80c98fce133d7bae219aacdd871
ilyas JOUBAER <joubaer.ilyas2006@gmail.com>
2026-09-23 14:30:47 +0200

    Modification du message de Main (Version I)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
-public static void main(String[] args) { System.out.println("Projet collaboratif - TP2 (version B)");
+public static void main(String[] args) { 
+    System.out.println("Projet collaboratif - TP2 (version I)");
 }
 }
