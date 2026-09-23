cec28292f77a39f8bba16241315a8797321b827b
ilyas JOUBAER <joubaer.ilyas2006@gmail.com>
2026-09-23 14:34:20 +0200

    Résolution du conflit sur Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
-<<<<<<< HEAD
 public static void main(String[] args) { 
     System.out.println("Projet collaboratif - TP2 (version I)");
-=======
 public static void main(String[] args) { System.out.println("Projet collaboratif - TP2 (version Y)");
->>>>>>> 9ed2f3c2bb42fca15c46f82f055c883503f28235
 }
 }
