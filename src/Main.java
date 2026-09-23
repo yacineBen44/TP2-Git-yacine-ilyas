5479964313b63fa37f48ab55db9cbfab34c3f5bf
Yacine Ben Ammar <yacine-benammar@hotmail.com>
2026-09-23 13:46:38 +0200

    Modification du message de Main (version A)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
  public static void main(String[] args) {
- System.out.println("Projet collaboratif - TP2");
- System.out.println("Version avec nouvelle fonctionnalité !");
+ System.out.println("Projet collaboratif - TP2 (version A)");
  }
 }
